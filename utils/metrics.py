b624c8b24cc6b8dbda650d997c42148e1c6dbaa4
New-Storyline <lubomir378@gmail.com>
2026-04-23 22:09:17 +0300

    add new metric RMSE

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,7 +1,5 @@
 
-
 import torch
-
 
 def L2_depth_loss(pred_depth, gd_depth):
     """
@@ -10,3 +8,10 @@ def L2_depth_loss(pred_depth, gd_depth):
     valid_mask = gd_depth > 0
     diff = pred_depth[valid_mask] - gd_depth[valid_mask]
     return torch.mean(diff ** 2)
+
+def RMSE_depth_metric(pred_depth, gd_depth):
+    """
+    RMSE loss calculated only on valid pixels (where gd_depth > 0)
+    """
+    mse = L2_depth_loss(pred_depth, gd_depth)
+    return torch.sqrt(mse)
